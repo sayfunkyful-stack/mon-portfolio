e32eec3625d8f6cb5e983f1f6259bc419b1c82db
Robin Schweizer - Git <sayfunkyful@gmail.com>
2026-03-18 14:51:36 +0100

    Ajout d'un titre responsive au hero

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -1,5 +1,8 @@
 import DarkVeil from './DarkVeil';
 import Navbar from './Navbar';
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap');
+</style>
 
 export default function Hero() {
   return (
